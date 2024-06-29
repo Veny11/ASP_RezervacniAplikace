4410e5607ac99ba3020a83661a90238cb43aaacb
Veny11 <dominik.venos@seznam.cz>
2024-06-29 16:18:15 +0200

    eh

--- Rezervace_Ples/obj/Debug/net7.0/Rezervace_Ples.AssemblyInfo.cs
+++ Rezervace_Ples/obj/Debug/net7.0/Rezervace_Ples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rezervace_Ples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79887f909ead63351c351e1d923d294dc22f7568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+573e3f83576eb3c11c66aa8c2a5dc2561367e799")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rezervace_Ples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rezervace_Ples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
